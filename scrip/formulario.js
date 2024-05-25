2c2afd115382e9a12a988e6facafe3582e539d4b
Ezequiel Hermoso <108770968+Ezequiel7100@users.noreply.github.com>
2024-05-24 22:41:33 -0300

    Final

--- scrip/formulario.js
+++ scrip/formulario.js
@@ -8,30 +8,30 @@
     const email = document.getElementById('email').value.trim();
     const telefono = document.getElementById('telefono').value.trim();
 
-    let isValid = true;
+    let validar = true;
     //NOMBRE
     if (nombre === '') {
-        mensajeError('nombre-error', 'Por favor, introduce tu nombre.');
-        isValid = false;
+        mensajeError('nombreError', 'Por favor, introduce tu nombre.');
+        validar = false;
     }
     if (!validarNombre(nombre)) {
-        mensajeError('nombre-error', 'El campo contiene numeros.');
-        isValid = false;
+        mensajeError('nombreError', 'El campo contiene numeros.');
+        validar = false;
     }
     //EMAIL
     if (!validarEmail(email)) {
-        mensajeError('email-error', 'Por favor, introduce un correo electrónico válido.');
-        isValid = false;
+        mensajeError('emailError', 'Por favor, introduce un correo electrónico válido.');
+        validar = false;
     }
 
     //TELEFONO
     if (!validarTelefono(telefono)) {
-        mensajeError('telefono-error', 'Por favor, introduce un número de teléfono válido.');
-        isValid = false;
+        mensajeError('telefonoError', 'Por favor, introduce un número de teléfono válido.');
+        validar = false;
     }
 
     //MOSTRAR 
-    if (isValid) {
+    if (validar) {
         const contactResponse = document.getElementById('contactMostrar');
         contactResponse.innerHTML = '';
 
@@ -40,9 +40,9 @@
         mensajeEnviado.style.color = 'green';
         contactResponse.appendChild(mensajeEnviado);
         
-        const message = document.createElement('p');
-        message.textContent = 'Nombre: ' + nombre + ', Correo Electrónico: ' + email + ', Teléfono: ' + telefono;
-        contactResponse.appendChild(message);
+        const mensaje = document.createElement('p');
+        mensaje.textContent = 'Nombre: ' + nombre + ', Correo Electrónico: ' + email + ', Teléfono: ' + telefono;
+        contactResponse.appendChild(mensaje);
 
         document.getElementById('contactForm').reset();
     }
@@ -67,7 +67,6 @@ function validarEmail(email) {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
 }
-
 function validarTelefono(telefono) {
     var re = /^\d{10}$/;
     return re.test(telefono);
